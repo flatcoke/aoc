f97b632bdb32083f8f1d700842082c8cec72eeda
Taemin Kim <flatcoke89@gmail.com>
2022-07-05 16:59:16 +0900

    set up

--- clojure/src/aoc2018_1.clj
+++ clojure/src/aoc2018_1.clj
@@ -1,9 +1,13 @@
-(ns aoc2018-1)
+(ns aoc2018-1
+  (:require [split-lines :as clojure.string]))
 
 ;; 파트 1
 ;; 주어진 입력의 모든 숫자를 더하시오.
 ;; 예) +10 -2 -5 +1 이 입력일 경우 4를 출력
 
+(reduce + (map #(Integer/parseInt %)
+               (clojure.string/split-lines
+                (slurp "./resources/aoc2018_1.txt"))))
 
 ;; 파트 2
 ;; 주어진 입력의 숫자를 더할 때 마다 나오는 숫자 중, 처음으로 두번 나오는 숫자를 리턴하시오.
