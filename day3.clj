e547bfe19278dc79f30398b86abe763b288429aa
Taemin Kim <flatcoke89@gmail.com>
2022-07-07 16:34:50 +0900

    EOF

--- day3.clj
+++
@@ -1,9 +0,0 @@
-(ns day3
-  (:require [clojure.java.io :as io]))
-
-(def input (-> "day3.sample.txt"
-               (io/resource)
-               (slurp)
-               (clojure.string/split-lines)))
-
-(count input)
