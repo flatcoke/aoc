a4e09f42e254186a4fa6b98f6b1e0d4c8f81c72f
Taemin Kim <flatcoke89@gmail.com>
2022-07-09 18:03:58 +0900

    Day 4 (#4)
    * 네째날
    
    * refact
    
    * EOF

--- clojure/src/aoc2018_4.clj
+++ clojure/src/aoc2018_4.clj
@@ -1,4 +1,6 @@
-(ns aoc2018_4)
+(ns aoc2018_4
+  (:require [clojure.string :as string]
+            [clojure.java.io :as io]))
 ;; 파트 1
 ;; 입력:
 
@@ -28,6 +30,116 @@
 ;; 파트 1은 “주어진 입력에 대해서, 가장 오랜시간 잠들어있었던 가드의 ID와, 그 가드가 가장 빈번하게 잠들어 있었던 분(minute)의 곱을 구하라”
 ;; 만약 20번 가드가 0시 10분~36분, 다음날 0시 5분~11분, 다다음날 0시 11분~13분 이렇게 잠들어 있었다면, “11분“이 가장 빈번하게 잠들어 있던 ‘분’. 그럼 답은 20 * 11 = 220.
 
+(defn get-sample-data [path]
+  (->> path
+       (io/resource)
+       (slurp)
+       (string/split-lines)
+       sort))
 
-;; 파트 2
-;; 주어진 분(minute)에 가장 많이 잠들어 있던 가드의 ID과 그 분(minute)을 곱한 값을 구하라.
+
+(defn parse-raw-line-to-map
+  "가공되지 않은 데이터를 특수문자 제거 후 Date, log 로 구분 
+     input: '[1518-05-24 23:57] Guard #523 begins shift'
+     output: [Date ('Guard' '523' 'begins' 'shift')]
+     "
+  [raw-s]
+  (let [[d t & log]      (-> raw-s
+                             (string/replace #"[\[\]#]" "")
+                             (string/split #" "))
+        [year month day] (string/split d #"-")
+        [hour min]       (string/split t #":")
+        action           (first log)
+        joined-log       (string/join #" " log)
+        guard-id         (when-let [guard-id (re-matches #"[0-9]+" (nth log 1))]
+                           (Integer/parseInt guard-id))]
+
+    {:year     (Integer/parseInt year)
+     :month    (Integer/parseInt month)
+     :day      (Integer/parseInt day)
+     :hour     hour
+     :min      (Integer/parseInt min)
+     :guard-id guard-id
+     :action   action
+     :log      joined-log}))
+
+
+(defn insert-guard-id-to-action
+  [logs]
+  (->> logs
+       (reduce (fn [acc m]
+                 (if-let [last-discoverd-guard-id (:guard-id m)]
+                   (assoc acc :guard-id last-discoverd-guard-id)
+                   (update acc :actions conj (assoc m :guard-id (:guard-id acc)))))
+               {:actions  []
+                :guard-id nil})
+       (:actions)))
+
+(comment
+  (defn compute-sleep-time
+    [actions]
+    (->> (partition 2 2 actions)
+         (map (fn  [[falls wakes]]
+                (let [guard-id    (:guard-id wakes)
+                      date        [(:year wakes) (:month wakes) (:day wakes)]
+                      wake-time   (:min wakes)
+                      fall-time   (:min falls)
+                      sleep-time  (- wake-time fall-time)
+                      all-minutes (range fall-time wake-time)]
+                  {:date        date
+                   :guard-id    guard-id
+                   :sleep-time  sleep-time
+                   :wake-time   wake-time
+                   :fall-time   fall-time
+                   :all-minutes all-minutes}))))))
+
+
+(comment
+  (defn compute-stats
+    [computed-map]
+    (->> (group-by :guard-id computed-map)
+         seq
+         (map (fn [[id m]]
+                {:guard-id       id
+                 :sum-sleep-time (->> m
+                                      (map (fn [obj] (:sleep-time obj)))
+                                      (reduce +))
+                 :all-minutes    (->> m
+                                      (map (fn [obj] (:all-minutes obj)))
+                                      flatten)})))))
+
+(comment
+  "day4 part1"
+  (->> (get-sample-data "aoc2018_4.txt")
+       (map parse-raw-line-to-map)
+       insert-guard-id-to-action
+       compute-sleep-time
+       compute-stats
+       (sort-by  :sum-sleep-time >)
+       first))
+
+
+(comment
+  "day4 part2"
+  (let [fall-wake-data (->> (get-sample-data "aoc2018_4.txt")
+                            (map parse-raw-line-to-map)
+                            insert-guard-id-to-action
+                            compute-sleep-time)
+        most-minute    (->>  fall-wake-data
+                             compute-stats
+                             (map :all-minutes)
+                             flatten
+                             frequencies
+                             (sort-by val #(compare %2 %1))
+                             ffirst)]
+    most-minute
+    (->> fall-wake-data
+         (group-by :guard-id)
+         (filter (fn [[_ d]]
+                   (= most-minute (->>  d
+                                        (map :all-minutes)
+                                        flatten
+                                        frequencies
+                                        (sort-by val >)
+                                        (map first)
+                                        first)))))))
