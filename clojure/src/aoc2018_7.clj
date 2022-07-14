8c363ad84d1a619f020d77b96a56763776cee656
Taemin Kim <flatcoke89@gmail.com>
2022-07-14 09:53:37 +0900

    temp

--- clojure/src/aoc2018_7.clj
+++ clojure/src/aoc2018_7.clj
@@ -97,8 +97,7 @@
         trace          (if (or (nil? current-step)
                                (contains? (set trace) current-step))
                          trace (conj trace current-step))]
-
-    (if (empty? can-do-steps) trace (generate-trace-step step-map trace))))
+    (if (empty? can-do-steps) trace (recur step-map trace))))
 
 (comment
   "part 1"
@@ -110,6 +109,10 @@
 
     (->> (generate-trace-step step-map [])
          string/join)))
+
+(comment
+  (->> (range)
+       (take 10)))
 
 ;; ## 파트 2
 
@@ -141,3 +144,214 @@
 ;;   14        E          .       CABFDE
 ;; ```
 ;; 15초가 걸리므로 답은 15
+(comment
+  (lazy-cat [1] [2]))
+
+(defn get-processing-second
+  "처리 시간을 반환합니다. 
+  input: A
+  output: 61
+  "
+  [step]
+  (- (int (.charAt step 0)) 4))
+
+(defn get-processing-second-test
+  "처리 시간을 반환합니다. 
+  input: A
+  output: 61
+  "
+  [step]
+  ;; (- (int (.charAt step 0)) 4))
+  (- (int (.charAt step 0)) 64))
+
+(defn create-workers
+  [worker-count]
+  (->> (range 1 (+ worker-count 1))
+       (map (fn [number] {:pk          number
+                          :step        nil
+                          ;; :start-time  0
+                          :finish-time 100}))))
+
+
+(defn find-idle-works
+  [workers]
+  (->> workers
+       (filter (fn [worker]
+                 (nil? (:step worker))))))
+
+(defn start-work
+  [workers current-time step]
+  (merge)
+  (conj (rest workers)
+        (assoc (first workers)
+               :step step
+               :finish-time (+ current-time (get-processing-second step)))))
+
+;; (defn clean-up-finished-steps
+;;   [workers current-time doing-steps done-steps]
+;;   filter workers
+
+
+;;   )
+
+(comment
+  (get-processing-second "A")
+  (get-processing-second-test "A")
+  (create-workers 5)
+  (find-idle-works (create-workers 5))
+  (start-work (create-workers 5) 0 "A"))
+
+(comment
+  (iterate inc 1))
+
+(defn generate-trace-step-by-worker
+  "들어온 리스트를 제외한 가능한 작업을 나열하고 우선순위가 높은 일감을 처리 하는 작업을 반복"
+  [step-map done-steps doing-steps finished-steps current-time workers]
+  (let [pre-step-map   (:pre-step-map step-map)
+        all-steps      (:all-chraters step-map)
+        all-left-steps (->> (set/difference all-steps (set done-steps)) sort)
+        can-do-steps   (->> (merge all-left-steps)
+                            (filter (fn [c]
+                                      (empty? (set/difference
+                                               (set (get pre-step-map c))
+                                               (set done-steps))))))
+        current-step   (first can-do-steps)
+        trace          (if (or (nil? current-step)
+                               (contains? (set done-steps) current-step))
+                         done-steps (conj done-steps current-step))
+        current-time   (inc current-time)]
+
+
+    #_(if (and (not (seq? can-do-steps)) ;; 처리 해야하는 일이 없음
+               (= (count (find-idle-work workers)) (count workers)) ;; 일하는 사람이 없음
+               (empty? doing-steps) ;; 진행되고 있는 일이 없음 
+               )[current-time trace]
+
+          (if (empty? can-do-steps) trace (generate-trace-step-by-worker step-map trace doing-steps finished-steps current-time workers)))))
+
+
+(comment
+  (map (fn [x y] [x y]) [1 2 3] [4 5 6 7]))
+
+(defn assign-works-to-iddle-workers
+  "일이 없는 워커에게 일을 할당함"
+  [workers steps current-time]
+
+  (loop [workers     workers
+         left-works  steps
+         doing-works []]
+    (if (or (empty? left-works)
+            (empty? (filter (fn [worker]
+                              (nil? (:step worker))) workers)))
+      [workers  left-works doing-works]
+
+      (let [iddle-workers         (->> workers
+                                       (filter (fn [worker]
+                                                 (nil? (:step worker)))))
+            first-work            (first left-works)
+            left-works            (rest left-works)
+
+            first-in-iddle-worker (assoc (first iddle-workers) :step first-work
+                                         :finished-time (+ (get-processing-second-test first-work)
+                                                           current-time))
+            doing-works           (conj doing-works first-work)
+            left-in-iddle-worker  (rest iddle-workers)
+            working-workers       (filter (fn [worker]
+                                            (not (nil? (:step worker))))
+                                          workers)
+            workers               (lazy-cat (list first-in-iddle-worker) left-in-iddle-worker working-workers)]
+
+        (recur workers left-works doing-works)))))
+
+
+(comment (assign-works-to-iddle-workers [{:id            1
+                                          :step          "A"
+                                          :finished-time 3100}
+                                         {:id            2
+                                          :step          nil
+                                          :finished-time nil}
+                                         {:id            3
+                                          :step          nil
+                                          :finished-time 0}]
+                                        ["Z" "D" "V"] 70))
+
+(defn clean-up-finished-steps-from-worker
+  [workers current-time]
+
+  (let [finished-steps (->> workers
+                            (filter (fn [worker]
+                                      (not (nil? (:step worker)))))
+                            (reduce (fn [acc worker]
+                                      (if (= current-time (:finished-time worker))
+                                        (conj acc (:step worker)) acc))
+                                    []))
+        workers        (map (fn [worker]
+                              (if (= current-time (:finished-time worker))
+                                (assoc worker :step nil :finished-time 0) worker)) workers)]
+
+    [workers finished-steps]))
+
+(comment (clean-up-finished-steps-from-worker [{:id            1
+                                                :step          "A"
+                                                :finished-time 71}
+                                               {:id            2
+                                                :step          "B"
+                                                :finished-time 73}
+                                               {:id            3
+                                                :step          "C"
+                                                :finished-time 74}]
+                                              71))
+
+
+(comment
+  (type (concat #{"A" "C" "B"} #{"A" "B" "C" "D"})))
+
+
+(defn taemin
+  [all-steps pre-step-map workers]
+
+  (loop [current-time 0
+         workers      workers
+         ;; 진행중 일감
+         doing-steps  #{}
+         ;; 완료된 일감
+         done-steps   #{}]
+
+    (if (= done-steps (set all-steps)) current-time
+        (let [can-do-steps                                (->> all-steps
+                                                               (filter (fn [c]
+                                                                         (empty? (set/difference
+                                                                                  (set (get pre-step-map c))
+                                                                                  done-steps))))
+                                                               (#(set/difference % (set doing-steps)))
+                                                               sort)
+              iddle-workers                               (find-idle-works workers)
+              startable-steps                             (->> (count iddle-workers))
+
+
+              [workers not-assigned-works assigned-works] (assign-works-to-iddle-workers workers can-do-steps current-time)
+              doing-steps                                 (assigned-works doing-steps)
+
+              ;; 완료된 일감을 정리한다.
+              [workers just-finished-steps]               (clean-up-finished-steps-from-worker workers current-time)
+              done-steps                                  (set/union done-steps just-finished-steps)]
+
+
+
+          (println (type assigned-works) (type doing-steps))
+          (println (type workers))
+        ;; 새로운 일감을 배정한다. 
+          (if (> 10 current-time)
+            (recur (inc current-time) workers  doing-steps [])
+            ;; (recur (inc current-time) (list workers) doing-steps done-steps)
+            ;; (recur (inc current-time) [] [] [])
+            current-time)))))
+
+(comment
+  "part 2"
+  (let [input        (->> (get-sample-data "aoc2018_7.txt")
+                          (map parse-steps))
+        pre-step-map (get-requirement-step-map input)
+        all-chraters (->> input flatten set)]
+
+    (->> (taemin all-chraters pre-step-map (create-workers 2)))))
